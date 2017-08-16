3db5770bfa4d3961500dbb53a3319048b5fc89ea
applededipan <sunjundedipan@163.com>
2017-08-16 09:09:36 +0800

    watchdog init move to last of the board init

--- ITSBS/drivers/board.c
+++ ITSBS/drivers/board.c
@@ -28,13 +28,14 @@ void boardInit(void)
 
 	timer2Init(1);
 	timer3Init(200);		
-	wdgInit();
-	
+
 	while (!getCpuId(&myNode.mac[0], &myNode.mac[1], &myNode.mac[2])) { //! if get mac failed, then continue to get till succeed!
 		ledFlashSet(1, 1000, 10);
 	}
 	
 	ledFlashSet(1, 1000, 10);
+	
+	wdgInit();	
 }
 
 
